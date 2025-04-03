19d0b54811bbe8f984d4e181386dcd84e979f8d1
pujan shrestha <shresthapujan769@gmail.com>
2025-04-03 21:26:17 +0545

    some changes

--- scripts/script.js
+++ scripts/script.js
@@ -95,23 +95,7 @@ function checkScroll() {
     });
 }
 
-// Back to top button
-const backToTop = document.getElementById('backToTop');
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        backToTop.classList.add('show');
-    } else {
-        backToTop.classList.remove('show');
-    }
-});
 
-backToTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
 
 // Initialize elements as invisible
 animateElements.forEach(element => {
